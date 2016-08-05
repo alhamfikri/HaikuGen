e4115df20f719aa8b34303d019d39f8a322950b8
Daniel Winterstein <daniel.winterstein@gmail.com>
2016-08-05 10:02:02 +0100

    Update haiku-format.js

--- code/haikugen/src/com/sodash/poetry/haiku-format.js
+++ code/haikugen/src/com/sodash/poetry/haiku-format.js
@@ -2,8 +2,8 @@
 	"@class": "Poem",
 	"format": "haiku",
 	"lines": [
-	          {"syllables": 5},
-	          {"syllables": 7},
-	          {"syllables": 5}
-	          ]
+		{"syllables": 5},
+		{"syllables": 7},
+		{"syllables": 5}
+	]
 }
