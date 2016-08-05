e4b62e01a89a37f3202edbcde34bb563bade5d2f
Daniel Winterstein <daniel.winterstein@gmail.com>
2016-08-05 10:01:33 +0100

    Shorter

--- code/haikugen/src/com/sodash/poetry/haiku-format.js
+++ code/haikugen/src/com/sodash/poetry/haiku-format.js
@@ -2,17 +2,8 @@
 	"@class": "Poem",
 	"format": "haiku",
 	"lines": [
-	          {
-	        	"@class": "Line",
-	        	"syllables": 5
-	          },
-	          {
-	        	"@class": "Line",
-	        	"syllables": 7
-	          },
-	          {
-	        	"@class": "Line",
-	        	"syllables": 5
-	          }
+	          {"syllables": 5},
+	          {"syllables": 7},
+	          {"syllables": 5}
 	          ]
 }
